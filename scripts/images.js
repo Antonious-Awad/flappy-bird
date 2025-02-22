22e5c9209f168c3ef6561febbab9b5fb6a3157d2
Antonious-Awad <antonuostony1@gmail.com>
2025-02-22 22:31:42 +0200

    fix: verify file path

--- scripts/images.js
+++ scripts/images.js
@@ -1,21 +1,21 @@
 // Load and export all game images
 export const backgroundImage = new Image();
-backgroundImage.src = "../images/flappybirdbg.png";
+backgroundImage.src = "/images/flappybirdbg.png";
 
 export const birdImage = new Image();
-birdImage.src = "../images/flappybird.png";
+birdImage.src = "/images/flappybird.png";
 
 export const topPipeImg = new Image();
-topPipeImg.src = "../images/toppipe.png";
+topPipeImg.src = "/images/toppipe.png";
 
 export const bottomPipeImg = new Image();
-bottomPipeImg.src = "../images/bottompipe.png";
+bottomPipeImg.src = "/images/bottompipe.png";
 
 export const playButtonImg = new Image();
-playButtonImg.src = "../images/flappyBirdPlayButton.png";
+playButtonImg.src = "/images/flappyBirdPlayButton.png";
 
 export const logoImg = new Image();
-logoImg.src = "../images/flappyBirdLogo.png";
+logoImg.src = "/images/flappyBirdLogo.png";
 
 export const gameOverImg = new Image();
-gameOverImg.src = "../images/flappy-gameover.png";
+gameOverImg.src = "/images/flappy-gameover.png";
